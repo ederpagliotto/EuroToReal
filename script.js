394a588598592dd990e04c9ddc857e5491bfb334
Eder Pagliotto <ederpagliotto@gmail.com>
2023-04-19 18:38:46 +0100

    testing function

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function convert() {
-    const euroInput = document.getElementById('value-input').value;
-    fetch('https://api.exchangerate-api.com/v4/latest/EUR')
-      .then(response => response.json())
-      .then(data => {
-        const rate = data.rates.BRL;
-        const realValue = euroInput * rate;
-        document.getElementById('result').innerHTML = `${euroInput} Euro = ${realValue.toFixed(2)} Real`;
-      })
-      .catch(error => console.error(error));
-  }
+  const inputEuro = document.getElementById('value-input').value
+  fetch('https://api.exchangerate-api.com/v4/latest/EUR')
+  .then(response => response.json())
+  .then(data => { 
+    const rate = data.rates.BRL
+    const resultConversion = inputEuro * rate 
+    document.getElementById('result').innerHTML = `${inputEuro} Euro(s) = ${resultConversion.toFixed('2')} Real(is)`
+  })
+}
