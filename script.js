17e961c4f78c7c90cd3998a5f7d59b92448ff02e
Eder Pagliotto <ederpagliotto@gmail.com>
2023-04-19 18:46:17 +0100

    error added

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function convert() {
-  const inputEuro = document.getElementById('value-input').value
+  const inputEuro = document.getElementById('value-input').value;
   fetch('https://api.exchangerate-api.com/v4/latest/EUR')
   .then(response => response.json())
   .then(data => { 
-    const rate = data.rates.BRL
-    const resultConversion = inputEuro * rate 
-    document.getElementById('result').innerHTML = `${inputEuro} Euro(s) = ${resultConversion.toFixed('2')} Real(is)`
+    const rate = data.rates.BRL;
+    const resultConversion = inputEuro * rate ;
+    document.getElementById('result').innerHTML = `${inputEuro} Euro(s) = ${resultConversion.toFixed('2')} Real(is)`;
   })
+  .catch(error => console.error(error));
 }
